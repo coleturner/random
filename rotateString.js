3b9126ae2a2bd9c7ba75c0056f6c73b740dc5495
Cole Turner <turner.cole@gmail.com>
2018-05-13 13:39:52 -0700

    Create rotateString.js

---
+++ rotateString.js
@@ -0,0 +1,66 @@
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+var rotateString = function(A, B) {
+    if (A === B) return true;
+    if (A.length !== B.length) return false
+    
+    const haystack = B.split('');
+    const needle = A.split('');
+    
+    let thread = needle[0];
+    let seeking = -1;
+    
+    const iterationIndexes = [];
+    let iteration = -1;
+    
+    for (let i = 0; i < haystack.length; i++) {
+        // If we're seeing and seeing a direct match, continue
+        if (seeking !== -1) {
+            if (needle[seeking] === haystack[i]) {
+                if (seeking < needle.length -1) {
+                    seeking++;
+                } else {
+                    break;
+                }
+            } else {
+                
+                // Found a mismatch, try an alternative path
+                if (iterationIndexes.length && iteration < iterationIndexes.length) {
+                    i = iterationIndexes[iteration];
+                    seeking = 1;
+                    iteration++;
+                    continue;
+                }
+
+                // Else we know there's nothing left
+                return false;
+            }
+        } else {
+            const matchesCurrentIndex = needle[0] === haystack[i];
+            const matchesSequential = matchesCurrentIndex && (i <= haystack.length - 2 && needle[1] === haystack[i + 1]);
+            const matchesFragment = matchesCurrentIndex && (i === haystack.length - 1 && needle[1] === haystack[0]);
+
+            if(matchesSequential || matchesFragment) {
+                if (seeking !== -1 && i > iterationIndexes[iterationIndexes.length -1]) {
+                    iterationIndexes.push(i);
+                } else {
+                    // We've found a precursory match
+                    seeking = 1;
+                }
+            }
+        }
+        
+        if (i >= haystack.length - 1) {
+            if (seeking !== -1) {
+                i = -1;
+            } else {
+                return false;
+            }
+        }
+    }
+    
+    return seeking !== -1;
+};
